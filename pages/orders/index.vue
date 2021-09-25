1619b467b0e8e599c2acdcd6ed41abe8deaeb0cf
hieptdbn99 <tdhmesut@gmail.com>
2021-09-25 11:47:39 +0700

    commit add product with order

--- pages/orders/index.vue
+++ pages/orders/index.vue
@@ -1,32 +1,32 @@
 <template>
   <v-data-table
-    :headers="headers"
-    :items="listOrder"
-    class="elevation-1"
+      :headers="headers"
+      :items="listOrder"
+      class="elevation-1"
   >
     <template #top>
       <v-toolbar
-        flat
+          flat
       >
         <v-toolbar-title>ORDERS LIST</v-toolbar-title>
         <v-divider
-          class="mx-4"
-          inset
-          vertical
+            class="mx-4"
+            inset
+            vertical
         />
-        <v-spacer />
+        <v-spacer/>
 
         <v-dialog
-          v-model="dialog"
-          max-width="500px"
+            v-model="dialog"
+            max-width="500px"
         >
           <template #activator="{ on, attrs }">
             <v-btn
-              color="primary"
-              dark
-              class="mb-2"
-              v-bind="attrs"
-              v-on="on"
+                color="primary"
+                dark
+                class="mb-2"
+                v-bind="attrs"
+                v-on="on"
             >
               New Item
             </v-btn>
@@ -41,78 +41,119 @@
               <v-container>
                 <v-row>
                   <v-col
-                    cols="12"
-                    sm="6"
-                    md="12"
+                      cols="12"
+                      sm="6"
+                      md="12"
                   >
                     <v-text-field
-                      v-model="editedItem.name_customer"
-                      label="Name Customer"
+                        v-model="editedItem.name_customer"
+                        label="Name Customer"
                     />
                   </v-col>
                   <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
+                      cols="12"
+                      sm="6"
+                      md="4"
                   >
                     <v-text-field
-                      v-model="editedItem.phone"
-                      label="Phone"
+                        v-model="editedItem.phone"
+                        label="Phone"
                     />
                   </v-col>
                   <v-col
-                    cols="12"
-                    sm="6"
-                    md="8"
+                      cols="12"
+                      sm="6"
+                      md="8"
                   >
                     <v-text-field
-                      v-model="editedItem.email"
-                      label="Email"
+                        v-model="editedItem.email"
+                        label="Email"
                     />
                   </v-col>
                   <v-col
-                    cols="12"
-                    sm="6"
-                    md="12"
+                      cols="12"
+                      sm="6"
+                      md="12"
                   >
                     <v-text-field
-                      v-model="editedItem.address"
-                      label="Address"
+                        v-model="editedItem.address"
+                        label="Address"
                     />
                   </v-col>
                   <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
+                      class="d-flex"
+                      cols="12"
+                      sm="4"
+                  >
+                    <v-select
+                        v-model="newProductSelect"
+                        :items="listProduct"
+                        item-text="name"
+                        item-value="id"
+                        label="Select Product"
+                        persistent-hint
+                        return-object
+                        single-line
+                    ></v-select>
+                  </v-col>
+                  <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
                   >
                     <v-text-field
-                      v-model="editedItem.total_product"
-                      label="Total Products"
-                      type="number"
-                      @keyup="changeTotalPrice"
+                        v-model="newProductAmount.total_product"
+                        label="Total Products"
+                        type="number"
                     />
                   </v-col>
                   <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
+                      cols="12"
+                      sm="6"
+                      md="1"
+                      class="mt-3"
                   >
-                    <v-text-field
-                      v-model="editedItem.price"
-                      label="Price"
-                      type="number"
-                      @keyup="changeTotalPrice"
-                    />
+                    <v-btn
+                        icon
+                        color="pink"
+                        @click="addProductOrder"
+                    >
+
+                      <v-icon>mdi-plus</v-icon>
+                    </v-btn>
                   </v-col>
+                  <template>
+                    <v-col
+                        cols="12"
+                        sm="12"
+                        md="12">
+                      <v-data-table
+                          :headers="headers_product"
+                          :items="listProductOrder"
+                          hide-default-footer
+                      >
+                        <template #item.actions="{ item }">
+                          <v-icon
+                              small
+                              @click="deleteProduct(item)"
+                          >
+                            mdi-delete
+                          </v-icon>
+                        </template>
+                      </v-data-table>
+                    </v-col>
+
+                  </template>
                   <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
+                      class="text-right"
+                      cols="12"
+                      sm="6"
+                      md="4"
                   >
                     <v-text-field
-                      v-model="editedItem.total_price"
-                      label="Total Price"
-                      disabled
+                        v-model="editedItem.total_price"
+                        label="Total Price"
+                        disabled
                     >
                       />
                     </v-text-field>
@@ -122,18 +163,18 @@
             </v-card-text>
 
             <v-card-actions>
-              <v-spacer />
+              <v-spacer/>
               <v-btn
-                color="blue darken-1"
-                text
-                @click="close"
+                  color="blue darken-1"
+                  text
+                  @click="close"
               >
                 Cancel
               </v-btn>
               <v-btn
-                color="blue darken-1"
-                text
-                @click="save"
+                  color="blue darken-1"
+                  text
+                  @click="save"
               >
                 Save
               </v-btn>
@@ -146,14 +187,14 @@
               Are you sure you want to delete this item?
             </v-card-title>
             <v-card-actions>
-              <v-spacer />
+              <v-spacer/>
               <v-btn color="blue darken-1" text @click="closeDelete">
                 Cancel
               </v-btn>
               <v-btn color="blue darken-1" text @click="deleteItemConfirm">
                 OK
               </v-btn>
-              <v-spacer />
+              <v-spacer/>
             </v-card-actions>
           </v-card>
         </v-dialog>
@@ -161,23 +202,23 @@
     </template>
     <template #item.actions="{ item }">
       <v-icon
-        small
-        class="mr-2"
-        @click="editItem(item)"
+          small
+          class="mr-2"
+          @click="editItem(item)"
       >
         mdi-pencil
       </v-icon>
       <v-icon
-        small
-        @click="deleteItem(item)"
+          small
+          @click="deleteItem(item)"
       >
         mdi-delete
       </v-icon>
     </template>
     <template #no-data>
       <v-btn
-        color="primary"
-        @click="initialize"
+          color="primary"
+          @click="initialize"
       >
         Reset
       </v-btn>
@@ -186,16 +227,28 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import {mapState} from 'vuex'
+
 export default {
 
-  async asyncData ({ store }) {
+  async asyncData({store}) {
     await Promise.all([
-      console.log('hello'),
       store.dispatch('order/getListProduct')
     ])
+
   },
   data: () => ({
+    headers_product: [
+      {
+        text: 'Name',
+        value: 'name',
+      },
+      {text: 'Total Product', value: 'total_product'},
+      {text: 'Price', value: 'price'},
+      {text: 'Total', value: 'total_price_pr'},
+      {text: 'Actions', value: 'actions', sortable: false},
+    ],
+
     dialog: false,
     dialogDelete: false,
     headers: [
@@ -205,23 +258,21 @@ export default {
         sortable: false,
         value: 'name_customer'
       },
-      { text: 'Phone', value: 'phone' },
-      { text: 'Email', value: 'email' },
-      { text: 'Address', value: 'address' },
-      { text: 'Total Product', value: 'total_product' },
-      { text: 'Price', value: 'price' },
-      { text: 'Total Price', value: 'total_price' },
-      { text: 'Actions', value: 'actions', sortable: false }
+      {text: 'Phone', value: 'phone'},
+      {text: 'Email', value: 'email'},
+      {text: 'Address', value: 'address'},
+      {text: 'Total Price', value: 'total_price'},
+      {text: 'Actions', value: 'actions', sortable: false}
     ],
+    editedProductIndex: -1,
     editedIndex: -1,
     editedItem: {
       name_customer: '',
       phone: '',
       email: '',
       address: '',
-      total_product: 0,
-      price: 0,
-      total_price: 0
+      arrayProduct: [],
+      total_price: 0,
     },
     defaultItem: {
       name_customer: '',
@@ -231,50 +282,76 @@ export default {
       total_product: 0,
       price: 0,
       total_price: 0
-    }
+    },
+    newProductSelect: {
+      id: 0,
+      name: '',
+      price: '',
+    },
+    newProductAmount: {
+      total_product: 0,
+      total_price_pr: 0,
+    },
+    listProductOrder: [],
   }),
 
   computed: {
-    formTitle () {
+    formTitle() {
       return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
     },
     ...mapState('order', ['listOrder', 'listProduct'])
-
   },
 
   watch: {
-    dialog (val) {
+    dialog(val) {
       val || this.close()
     },
-    dialogDelete (val) {
+    dialogDelete(val) {
       val || this.closeDelete()
     }
   },
 
-  created () {
+  created() {
     this.initialize()
   },
 
   methods: {
-    initialize () {
+    initialize() {
       this.$store.dispatch('order/getListOrder')
     },
-    changeTotalPrice () {
-      this.editedItem.total_price = this.editedItem.total_product * this.editedItem.price
-    },
-    editItem (item) {
+    editItem(item) {
       this.editedIndex = this.listOrder.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialog = true
     },
 
-    deleteItem (item) {
+    deleteItem(item) {
       this.editedIndex = this.listOrder.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialogDelete = true
     },
+    addProductOrder() {
+      this.newProductAmount.total_price_pr = parseInt(this.newProductAmount.total_product) * parseInt(this.newProductSelect.price)
+      this.listProductOrder.push(Object.assign(this.newProductSelect, this.newProductAmount))
+      this.editedItem.total_price += this.newProductAmount.total_price_pr
+      this.newProduct = {
+        id: 0,
+        name: '',
+        price: '',
+      }
+      this.newProductAmount = {
+        total_product: 0,
+        total_price_pr: '',
+      }
+      console.log(this.listProductOrder)
+    },
+    deleteProduct(item){
+      this.editedProductIndex = this.listProductOrder.indexOf(item)
+      this.editedItem.total_price -= this.listProductOrder[this.editedProductIndex].total_price_pr
+      this.listProductOrder.splice(this.editedProductIndex , 1)
 
-    deleteItemConfirm () {
+    },
+    deleteItemConfirm() {
       const order = {
         orderInfo: this.editedItem,
         index: this.editedIndex
@@ -283,7 +360,7 @@ export default {
       this.closeDelete()
     },
 
-    close () {
+    close() {
       this.dialog = false
       this.$nextTick(() => {
         this.editedItem = Object.assign({}, this.defaultItem)
@@ -291,7 +368,7 @@ export default {
       })
     },
 
-    closeDelete () {
+    closeDelete() {
       this.dialogDelete = false
       this.$nextTick(() => {
         this.editedItem = Object.assign({}, this.defaultItem)
@@ -299,7 +376,7 @@ export default {
       })
     },
 
-    save () {
+    save() {
       if (this.editedIndex > -1) {
         const order = {
           orderInfo: this.editedItem,
@@ -307,6 +384,7 @@ export default {
         }
         this.$store.dispatch('order/updateOrder', order)
       } else {
+        this.editedItem.arrayProduct.push(this.listProductOrder);
         this.$store.dispatch('order/addOrder', this.editedItem)
       }
       this.close()
--- store/order.js
+++ store/order.js
@@ -1,74 +1,76 @@
 export const state = () => ({
-  listOrder: [],
-  listProduct: []
+    listOrder: [],
+    listProduct: []
 })
 
 export const mutations = {
-  setListOrder (state, orders) {
-    state.listOrder = orders
-  },
-  pushOrder (state, order) {
-    state.listOrder.push(order.data)
-  },
-  updateOrder (state, order) {
-    Object.assign(state.listOrder[order.index], order.orderUpdate.data)
-  },
-  deleteOrder (state, index) {
-    state.listOrder.splice(index, 1)
-  },
-  getListProduct (state, products) {
-    state.listProduct = products
-  }
+    setListOrder(state, orders) {
+        state.listOrder = orders
+    },
+    pushOrder(state, order) {
+        state.listOrder.push(order)
+    },
+    updateOrder(state, order) {
+        Object.assign(state.listOrder[order.index], order.orderUpdate.data)
+    },
+    deleteOrder(state, index) {
+        state.listOrder.splice(index, 1)
+    },
+    getListProduct(state, products) {
+        state.listProduct = products.data
+    }
 
 }
 
 export const actions = {
-  async getListOrder ({ commit }) {
-    await this.$axios.get('api/orders').then((res) => {
-      console.log(res)
-      commit('setListOrder', res.data)
-    }).catch((err) => {
-      console.log(err)
-    })
-  },
-  async addOrder ({ commit }, order) {
-    console.log(order)
-    await this.$axios.post('api/orders', order).then((res) => {
-      commit('pushOrder', res.data)
-    }).catch((err) => {
-      console.log(err)
-    })
-  },
-  async updateOrder ({ commit }, order) {
-    console.log(order.orderInfo)
-    await this.$axios.put('api/orders/' + order.orderInfo.id, order.orderInfo).then((res) => {
-      const updateItem = {
-        orderUpdate: res.data,
-        index: order.index
-      }
-      commit('updateOrder', updateItem)
-    }).catch((err) => {
-      console.log(err)
-    })
-  },
-  async deleteOrder ({ commit }, order) {
-    console.log(order.orderInfo)
-    await this.$axios.delete('api/orders/' + order.orderInfo.id).then((res) => {
-      const updateItem = {
-        orderUpdate: res.data,
-        index: order.index
-      }
-      commit('deleteOrder', updateItem)
-    }).catch((err) => {
-      console.log(err)
-    })
-  },
-  async getListProduct ({ commit }) {
-    await this.$axios.get('api/products').then((res) => {
-      commit('getListProduct', res.data || [])
-    }).catch((err) => {
-      console.log(err)
-    })
-  }
+    async getListOrder({commit}) {
+        await this.$axios.get('api/orders').then((res) => {
+            console.log(res)
+            commit('setListOrder', res.data)
+        }).catch((err) => {
+            console.log(err)
+        })
+    },
+    async addOrder({commit}, order) {
+        console.log(order)
+        await this.$axios.post('api/orders', order).then((res) => {
+            console.log(res.data)
+            commit('pushOrder', res.data)
+        }).catch((err) => {
+            console.log(err)
+        })
+    },
+    async updateOrder({commit}, order) {
+        console.log(order.orderInfo)
+        await this.$axios.put('api/orders/' + order.orderInfo.id, order.orderInfo).then((res) => {
+            const updateItem = {
+                orderUpdate: res.data,
+                index: order.index
+            }
+            commit('updateOrder', updateItem)
+        }).catch((err) => {
+            console.log(err)
+        })
+    },
+    async deleteOrder({commit}, order) {
+        console.log(order.orderInfo)
+        await this.$axios.delete('api/orders/' + order.orderInfo.id).then((res) => {
+            const updateItem = {
+                orderUpdate: res.data,
+                index: order.index
+            }
+            commit('deleteOrder', order.index)
+        }).catch((err) => {
+            console.log(err)
+        })
+    },
+    async getListProduct({commit}) {
+        await this.$axios.get('api/products').then((res) => {
+            commit('getListProduct', res.data || [])
+            console.log(res.data)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
 
 }
